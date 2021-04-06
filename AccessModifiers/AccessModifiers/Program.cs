127c39d42b2a77e0792c3408b3f093db7f3152f8
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2021-04-06 07:09:20 +0400

    Add files via upload

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using Demo;
+using Demo2.DemoInDemo2;
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Access Modifiers
+
+            #region Public - class & all class members(field,property,method)
+            //Car car = new Car();
+            //car.Brend = "Mercedes";
+            //Console.WriteLine(car.Brend);
+            #endregion
+
+            #region Private-all class members
+            //Car car = new Car();
+            //car.Speed = 300;
+            //Console.WriteLine(car.Speed);
+            //car.SetSpeed(300);
+            //Console.WriteLine(car.GetSpeed());
+            //Console.WriteLine(car.Color);
+            #endregion
+
+            #region Protected - all class members
+            //Car car = new Car();
+            //car.harsePower = 300;
+            #endregion
+
+            #region public readonly,private readonly - field
+            //Car car = new Car();
+            //Console.WriteLine(car.Price);
+            #endregion
+
+            #endregion
+
+            #region Namespace
+            Demo.Car car = new Demo.Car();
+            Test car1 = new Test();
+            Person person = new Person();
+            Console.WriteLine();
+            #endregion
+        }
+    }
+}
